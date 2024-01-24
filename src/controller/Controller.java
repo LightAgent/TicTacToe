1e153ed6af9ead0fa6a048b247190faade49ea36
Ahmed Zaki <midozaki569@gmail.com>
2024-01-24 20:44:38 +0200

    Modifed controller & added single structure

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,5 +1,21 @@
 package controller;
+import model.XOBoard;
 
-public interface Controller {
-    
+public abstract class Controller {
+    protected boolean xTurn;
+    protected boolean gameEnded;
+    protected XOBoard board = new XOBoard();
+    protected int validSqu = 9;
+    protected boolean checkWinner(XOBoard board) {
+
+        for (int i = 0; i < 3; i++) {
+            if (board.getMarker(i, 0) == board.getMarker(i, 1) && board.getMarker(i, 1) == board.getMarker(i, 2) ||
+                    board.getMarker(0, i) == board.getMarker(1, i) && board.getMarker(0, i) == board.getMarker(2, i))
+                return true;
+        }
+        if (board.getMarker(0, 0) == board.getMarker(1, 1) && board.getMarker(1, 1) == board.getMarker(2, 2) ||
+                board.getMarker(0, 2) == board.getMarker(1, 1) && board.getMarker(0, 2) == board.getMarker(2, 0))
+            return true;
+        return false;
+    }
 }
